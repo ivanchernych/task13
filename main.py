6dc1bc9a559d3de3a7d177dffc4f6cd78e5fd60d
ivan <d1zzzy.project@gmail.com>
2023-03-23 21:49:45 +0300

    0_0

--- main.py
+++ main.py
@@ -10,42 +10,37 @@ photo_file = "static/img/photo.jpg"
 
 
 @app.route('/')
-@app.route('/load_photo', methods=['POST', 'GET'])
-def load_photo():
-    if request.method == 'GET':
-        return f'''<!doctype html>
-                        <html lang="en">
-                          <head>
-                            <meta charset="utf-8">
-                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                            <link rel="stylesheet"
-                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                            crossorigin="anonymous">
-                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                            <title>Отбор астронавтов</title>
-                          </head>
-                          <body>
-                            <h1>Загрузка фотографии</h1>
-                            <h2>для участия в миссии</h2>
-                            <div>
-                                <form class="login_form" method="post" enctype="multipart/form-data">
-                                    <div class="form-group">
-                                        <label for="photo">Приложите фотографию</label>
-                                        <input type="file" class="form-control-file" id="photo" name="file">
-                                    </div>
-                                    <button type="submit" class="btn btn-primary">Отправить</button>
-                                    <img src="{url_for('static', filename='img/res.jpg')}" 
-                                    alt="здесь должна была быть картинка, но вы ее пока не загрузили">
-                                </form>
-                            </div>
-                          </body>
-                        </html>'''
-    elif request.method == 'POST':
-        f = request.files['file']
-        image = Image.open(BytesIO(f.read()))
-        image.save('static/img/res.jpg')
-        return "Форма отправлена"
+@app.route('/choice/<planet_name>')
+def choice(planet_name):
+    return f"""<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                        <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                        <title>Привет, Марс!</title>
+                      </head>
+                      <body>
+                        <h1>Мое предложение: {planet_name}</h1>
+                        <h2>Эта планета близка к Земле;</h2>
+                        <div class="alert alert-success" role="alert">
+                          На ней много необходимых ресурсов;
+                        </div>
+                        <div class="alert alert-success" role="alert">
+                          На ней есть вода и атмосфера;
+                        </div>
+                        <div class="alert alert-warning" role="alert">
+                         На ней есть небольшое магнитное поле;
+                        </div>
+                        <div class="alert alert-danger" role="alert">
+                          Наконец, она просто красива!
+                        </div>
+                      </body>
+                    </html>"""
 
 
 if __name__ == '__main__':
